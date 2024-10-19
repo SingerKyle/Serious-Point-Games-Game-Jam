56ecb24a50d1ee6cd678a69582de6a3fcca307e2
SingerKyle <98783661+SingerKyle@users.noreply.github.com>
2024-10-19 16:00:26 +0100

    Platform for collision and swim testing

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+// -------------------------------- Mechanics--------------------------------------
+// Oxygen Meter - remove and add over time
+// swim bool
+// 
+//
+// --------------------------------------------------------------------------------
+
 public class PlayerController : MonoBehaviour
 {
     // Variables for editor adjustment
     //SerializeField] float m_jumpForce = 400f;
-    [SerializeField] float m_movementSpeed = 400f; //public Vector2 m_movementSpeed = new Vector2(200f,200f);
+    [SerializeField] float m_movementSpeed = 20f;
+    [SerializeField] float m_swimmingSpeed = 20f;
     [Range(0, .3f)][SerializeField] private float m_movementSmooth = 0.125f;
 
 
@@ -16,6 +24,11 @@ public class PlayerController : MonoBehaviour
     float m_verticalInput;
     Vector2 m_moveDirection;
     Vector2 m_currentVelocity = Vector2.zero;
+   
+
+    bool m_isSwimming;
+
+    bool m_isGrounded;
 
     // Physics Body
     public Rigidbody2D rb;
@@ -25,6 +38,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
 
+        m_isSwimming = false;
     }
 
     // Update is called once per frame
@@ -37,6 +51,12 @@ public class PlayerController : MonoBehaviour
     {
         // Use this function for physics calculations Mikey :)
         MovePlayer();
+
+        if (m_isGrounded)
+        {
+            
+        }
+
     }
 
     private void PlayerInput()
@@ -55,5 +75,22 @@ public class PlayerController : MonoBehaviour
         Vector2 targetVelocity = m_moveDirection * m_movementSpeed;
 
         rb.velocity = Vector2.SmoothDamp(rb.velocity, targetVelocity, ref m_currentVelocity, m_movementSmooth);
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            m_isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            m_isGrounded = false;
+        }
     }
 }
