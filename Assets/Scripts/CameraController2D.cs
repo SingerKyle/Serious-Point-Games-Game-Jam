2476a69da68a80451cf68a78b440eee0dfa76817
SingerKyle <98783661+SingerKyle@users.noreply.github.com>
2024-10-19 14:58:56 +0100

    Adjustment of Camera Controller - removed bounding

--- Assets/Scripts/CameraController2D.cs
+++ Assets/Scripts/CameraController2D.cs
@@ -9,12 +9,9 @@ public class CameraController2D : MonoBehaviour
     [Range(0, 5)][SerializeField] public float m_yOffset;
     public Transform m_target;
 
-    private float s_screenWidth;
-
     // Start is called before the first frame update
     void Start()
     {
-        s_screenWidth = transform.position.x;
 
     }
 
@@ -26,28 +23,12 @@ public class CameraController2D : MonoBehaviour
 
     private void FixedUpdate()
     {
-        /*if (m_target != null) 
+        if (m_target != null )
         {
             Vector3 newPos = new Vector3(m_target.position.x, m_target.position.y + m_yOffset, transform.position.z);
 
-            transform.position = Vector3.Slerp(transform.position, newPos, m_followSpeed*Time.deltaTime);
-
-        }*/
-
-        if (m_target != null)
-        {
-            if (m_target.position.x > s_screenWidth)
-            {
-                Vector3 newPos = new Vector3(m_target.position.x, m_target.position.y + m_yOffset, transform.position.z);
-
-                transform.position = Vector3.Slerp(transform.position, newPos, m_followSpeed * Time.deltaTime);
-                //transform.position = new Vector3(m_target.position.x, transform.position.y, transform.position.z);
-            }
-            else
-            {
-                Vector3 newPos = new Vector3(s_screenWidth, m_target.position.y + m_yOffset, transform.position.z);
-                transform.position = Vector3.Slerp(transform.position, newPos, m_followSpeed * Time.deltaTime);
-            }
+            transform.position = Vector3.Slerp(transform.position, newPos, m_followSpeed * Time.deltaTime);
+            //transform.position = new Vector3(m_target.position.x, transform.position.y, transform.position.z);
         }
     }
 }
